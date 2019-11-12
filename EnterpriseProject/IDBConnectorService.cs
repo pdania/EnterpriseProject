1f95ac44bf8b997c8f3c2ee438a104343f78d4b6
Danylo <pdania@ukr.net>
2019-11-12 12:45:22 +0200

    Add project files.

---
+++ EnterpriseProject/IDBConnectorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace EnterpriseProject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IDBConnectorService" in both code and config file together.
+    [ServiceContract]
+    public interface IDBConnectorService
+    {
+        [OperationContract]
+        string AddUser(string user);
+    }
+}
